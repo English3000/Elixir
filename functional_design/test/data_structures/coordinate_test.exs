f66560726d4fce8d5b21a7ed660b8feff1049ee9
Alexander <english3000.org@gmail.com>
2018-07-16 17:25:52 -0700

    + :stream_data => use ExUnitProperties

--- functional_design/test/data_structures/coordinate_test.exs
+++ functional_design/test/data_structures/coordinate_test.exs
@@ -1,19 +1,18 @@
 defmodule DataStructures.CoordinateTest do
   use ExUnit.Case, async: true
-  use ExUnitProperties # "module ExUnitProperties is not loaded and could not be found"
-  alias IslandsEngine.DataStructures
+  use ExUnitProperties
+  alias IslandsEngine.DataStructures.Coordinate
 
   IO.puts "Coordinate.new/3"
-
   property "given valid range, handles row & col inputs properly" do
     check all        first <- integer(1..5),
                       last <- integer(6..10),
-                  in_range <- integer(1..5),
-                   non_int <- integer(-4..0),
-              not_in_range <- integer(11..15) do
-      assert {:ok, _}    = DataStructures.Coordinate.new(in_range, in_range, first..last)
-      assert {:error, _} = DataStructures.Coordinate.new(non_int, in_range, first..last)
-      assert {:error, _} = DataStructures.Coordinate.new(in_range, not_in_range, first..last)
+                  in_range <- integer(first..last),
+                   non_int <- integer(first * -1..0),
+              not_in_range <- integer(last + 1..last * 2) do
+      assert {:ok, _}    = Coordinate.new(in_range, in_range, first..last)
+      assert {:error, _} = Coordinate.new(non_int, in_range, first..last)
+      assert {:error, _} = Coordinate.new(in_range, not_in_range, first..last)
     end
   end
 end
---
+++ functional_design/test/data_structures/guesses_test.exs
@@ -0,0 +1,10 @@
+defmodule DataStructures.GuessesTest do
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+  alias IslandsEngine.DataStructures.Guesses
+
+  IO.puts "Guesses.add/3"
+  property "handles hits & misses" do
+    #
+  end
+end
