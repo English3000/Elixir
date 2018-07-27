582c83e036d4c8fe770a70be77ef649a331973bd
Alexander <english3000.org@gmail.com>
2018-07-27 16:00:04 -0700

    bug adding :islands_engine to :islands_interface (Phoenix app)

--- functional_design/lib/islands_engine/application.ex
+++ functional_design/lib/islands_engine/application.ex
@@ -1,8 +1,6 @@
 defmodule IslandsEngine.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
+  # See https://hexdocs.pm/elixir/Application.html for more information on OTP Applications
   @moduledoc false
-
   use Application
 
   def start(_type, _args) do
@@ -10,10 +8,11 @@ defmodule IslandsEngine.Application do
     children = [ {Registry, keys: :unique, name: Registry.Game},
                  IslandsEngine.Game.Supervisor ]
 
+    :dets.open_file(:game, [])
     # :ets.new(:game, [:public, :named_table])
-    :dets.open_file(:game, []) # TEST!
 
     opts = [strategy: :one_for_one, name: IslandsEngine.Supervisor]
+
     Supervisor.start_link(children, opts)
   end
 end
--- functional_design/mix.exs
+++ functional_design/mix.exs
@@ -1,28 +1,17 @@
 defmodule IslandsEngine.MixProject do
-  use Mix.Project
+  use Mix.Project # https://hexdocs.pm/mix/Mix.Project.html
 
-  def project do
-    [
-      app: :islands_engine,
-      version: "0.1.0",
-      elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
+  def project, # metadata
+    do: [ app: :islands_engine,
+          version: "0.1.0",
+          elixir: "~> 1.6",
+          start_permanent: Mix.env() == :prod,
+          deps: deps() ]
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {IslandsEngine.Application, []}
-    ]
-  end
+  defp deps, # compile-time deps
+    do: [ {:stream_data, "~> 0.1", only: :test} ] # Run "mix help deps" to learn about dependencies.
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      {:stream_data, "~> 0.1", only: :test}
-    ]
-  end
+  def application, # run-time deps
+    do: [ extra_applications: [:logger],
+          mod:                {IslandsEngine.Application, []} ] # Run "mix help compile.app" to learn about applications.
 end
