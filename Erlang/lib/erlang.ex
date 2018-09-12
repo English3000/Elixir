7bfd919a6ec1616e64823175e07dc3e95f0c12c3
Alexander <english3000.org@gmail.com>
2018-09-11 21:27:40 -0700

    @ Ch. 5: Records && Maps

--- Erlang/lib/erlang.ex
+++ Erlang/lib/erlang.ex
@@ -6,33 +6,72 @@ defmodule Erlang do
   [3, 2] ++ [1] == [3, 2 | [1]]
   """
 
-  defmodule List do
+  defmodule Lists do
     def map(output, [], _),
       do: output
     def map(output, [head | tail], function),
       do: map(output ++ [function.(head)], tail, function)
 
-  end
-
-  defmodule Enum do
-    def map(enum, function),
-      do: for item <- enum, do: function.(item)
-      # erl: [function.(item) || item <- list]
-
+    # `for` fn's
+      # `:into` https://elixir-lang.org/getting-started/comprehensions.html#the-into-option
     def quicksort([]),
       do: []
-    def quicksort([nil | tail]),
-      do: tail
     def quicksort([pivot | tail]) do
-      quicksort(for item <- tail, do:
-                  if item < pivot, do: item) ++
+      quicksort(for item <- tail,
+                    item < pivot,
+                      do: item) ++
 
       [pivot] ++
 
-      quicksort(for item <- tail, do:
-                  if item >= pivot, do: item)
-
-      |> Enum.reject(&(&1 == nil))
+      quicksort(for item <- tail,
+                    item >= pivot,
+                      do: item)
     end
+
+    def pythagorean_triplets(n),
+      do: for a <- 1..n,
+              b <- 1..n,
+              c <- 1..n,
+              a + b + c <= n,
+              a*a + b*b == c*c,
+                do: {a, b, c}
+
+    def permutations([]),
+      do: [[]]
+    def permutations(charlist),
+      do: for head <- charlist,
+              tail <- permutations(charlist -- [head]),
+                do: [head | tail]
+
+    def odds_and_evens(list),
+      do: odds_and_evens(list, [], [])
+    defp odds_and_evens([], odds, evens),
+      do: {Enum.reverse(odds), Enum.reverse(evens)}
+    defp odds_and_evens([head | tail], odds, evens) do
+      case rem(head, 2) do
+        1 -> odds_and_evens(tail, [head | odds], evens)
+        0 -> odds_and_evens(tail, odds, [head | evens])
+      end
+    end
+
+    def filter(list, function),
+      do: for item <- list,
+              function.(item) == true,
+                do: item
+  end
+
+  defmodule Enums do
+    def map(enum, function),
+      do: for item <- enum, do: function.(item)
+      # erl: [function.(item) || item <- list]
+  end
+
+  defmodule Tuples do
+    def to_list(tuple),
+      do: to_list(tuple, 0, [])
+    defp to_list(tuple, index, output) when index < tuple_size(tuple),
+      do: to_list(tuple, index + 1, [elem(tuple, index) | output])
+    defp to_list(_tuple, _index, output),
+      do: Enum.reverse(output)
   end
 end
--- Erlang/test/erlang_test.exs
+++ Erlang/test/erlang_test.exs
@@ -2,7 +2,14 @@ defmodule ErlangTest do
   use ExUnit.Case
   doctest Erlang
 
-  test "" do
-    # assert Erlang.hello() == :world
+  test "pythagorean_triplets" do
+    assert Erlang.Lists.pythagorean_triplets(16) == [{3,4,5}, {4,3,5}]
+    assert Erlang.Lists.pythagorean_triplets(30) == [ {3,4,5},  {4,3,5},  {5,12,13},
+                                                      {6,8,10}, {8,6,10}, {12,5,13} ]
+  end
+
+  test "permutations" do
+    assert Erlang.Lists.permutations('123') == [ '123', '132', '213',
+                                                 '231', '312', '321' ]
   end
 end
