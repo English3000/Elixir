368b7010e2ddd2a785eaec1b7b8af1b7f8d341ec
Alexander Marks-Katz <alexander.marks-katz@hq-m109559.trr.local>
2018-09-28 16:55:11 -0700

    ...

--- Erlang/lib/8_files/helper.ex
+++ Erlang/lib/8_files/helper.ex
@@ -10,11 +10,25 @@ defmodule Helper do # @ L 6757
   end
   # h.s., WORKS!
   @doc "Writes a `list` of Elixir terms to `file`"
-  def write(list, file) do # `File.write` is more efficient b/c only 1 op
-    {:ok, pid} = File.open("lib/8_files/" <> file, [:write])
+  def write(list, path) do # `File.write` is more efficient b/c only 1 op
+    {:ok, pid} = File.open("lib/8_files/" <> path, [:write])
     Enum.each( list, &(IO.inspect(pid, &1, [])) )
     File.close(pid)
   end
+
+  def file_info(path) do
+    case File.stat(path) do
+      {:ok, info} -> {info.type, info.size} # :filelib.size/1, :filelib.is_dir/1
+                _ -> :error
+    end
+  end
+
+  def dir_info(path) do
+    {:ok, dirs} = File.ls
+    Enum.sort(dirs) |> Enum.map( &({&1, file_info(&1)}) )
+  end
+
+  def find(path, regex, recursive, function, acc) # L 6866
 end
 
 # Helper.print_by_line(pid)
