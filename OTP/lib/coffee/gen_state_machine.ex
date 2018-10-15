09fd728cb4d4890b23db78c479f270b6dd965644
Alexander Marks-Katz <english3000.org@gmail.com>
2018-10-15 16:33:56 -0700

    ...

---
+++ OTP/lib/coffee/gen_state_machine.ex
@@ -0,0 +1,2 @@
+# https://hexdocs.pm/gen_state_machine/GenStateMachine.html
+# @ L 3357
--- OTP/mix.exs
+++ OTP/mix.exs
@@ -14,13 +14,14 @@ defmodule OTP.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [:gen_state_machine],
       extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
+    [ {:gen_state_machine, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
