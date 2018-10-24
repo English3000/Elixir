13f478148a3d5c5cd8bcfd3f20730fffaf4b72c6
Alexander <english3000.org@gmail.com>
2018-10-23 22:02:30 -0700

    Δ converted to umbrella project

---
+++ functional_design/islands/README.md
@@ -0,0 +1,4 @@
+# Islands
+
+**TODO: Add description**
+
--- functional_design/islands/apps/islands_engine/mix.exs
+++ functional_design/islands/apps/islands_engine/mix.exs
@@ -4,13 +4,19 @@ defmodule IslandsEngine.MixProject do
   def project, # metadata
     do: [ app: :islands_engine,
           version: "0.1.0",
-          elixir: "~> 1.6",
+          elixir: "~> 1.7",
+          build_path: "​​../../_build",
+          config_path: "​​../../config/config.exs",
+          deps_path: "../../deps",
+          lockfile: "../../mix.lock",
+          build_embedded: Mix.env() == :prod,
           start_permanent: Mix.env() == :prod,
           deps: deps() ]
 
   defp deps, # compile-time deps
-    do: [ {:stream_data, "~> 0.1", only: :test}, # Run "mix help deps" to learn about dependencies.
-          {:shorthand, "~> 0.0.3"} ] 
+    do: [ {:distillery, "~> 2.0"}, # Run "mix help deps" to learn about dependencies.
+          {:stream_data, "~> 0.1", only: :test},
+          {:shorthand, "~> 0.0.3"} ]
 
   def application, # run-time deps
     do: [ extra_applications: [:logger],
