73d750d25ed2c4c315ec0ac123f0806090696cb9
Alexander <english3000.org@gmail.com>
2018-10-23 20:57:58 -0700

    + pre-SSR commit hash

--- functional_design/islands_engine/mix.exs
+++ functional_design/islands_engine/mix.exs
@@ -1,3 +1,4 @@
+# for pre-SSR, git revert 1a2f3e2b3ced8c3faadfcdc4afdca8e99fa876aa
 defmodule IslandsEngine.MixProject do
   use Mix.Project # https://hexdocs.pm/mix/Mix.Project.html
 
@@ -10,7 +11,7 @@ defmodule IslandsEngine.MixProject do
 
   defp deps, # compile-time deps
     do: [ {:stream_data, "~> 0.1", only: :test}, # Run "mix help deps" to learn about dependencies.
-          {:shorthand, "~> 0.0.3"} ] 
+          {:shorthand, "~> 0.0.3"} ]
 
   def application, # run-time deps
     do: [ extra_applications: [:logger],
