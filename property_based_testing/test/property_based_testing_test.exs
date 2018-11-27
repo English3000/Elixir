802fe2755546d64c3b94023dec9e73130850a132
Alexander Marks-Katz <english3000.org@gmail.com>
2018-11-26 16:28:25 -0800

    + Ch. 3 exercises, > collect/2

--- property_based_testing/test/property_based_testing_test.exs
+++ property_based_testing/test/property_based_testing_test.exs
@@ -80,4 +80,60 @@ defmodule PropertyBasedTestingTest do
       end
     end
   end
+
+  # `mix test --only exercise`
+  # `mix test help`
+  @tag exercise: :Ch_3
+  describe "~ MapSet.union/2" do
+    property "works" do
+      forall {list1, list2} <- {list(any), list(any)} do
+        list1 ++ list2 |> Enum.uniq |> Enum.sort()
+          ==
+        MapSet.union( MapSet.new(list1), MapSet.new(list2) ) |> MapSet.to_list |> Enum.sort()
+      end
+    end
+  end
+
+  @tag exercise: :Ch_3
+  describe "~ Map.merge/3" do # doesn't test the function?? doesn't test multiple layers?? # No, only validates keys.
+    property "has good coverage" do
+      forall {list1, list2} <- {list({any, any}), list({any, any})} do
+        Map.merge( Map.new(list1), Map.new(list2), fn _k, v1, _v2 -> v1 end)
+        |> Map.to_list
+        |> Enum.sort
+        |> extract_keys()
+          ==
+        list1 ++ list2
+        |> extract_keys
+        |> Enum.uniq
+        |> Enum.sort()
+      end
+    end
+  end
+
+  defp extract_keys(list), do: (for {k, _} <- list, do: k)
+
+  property "Introducing, collect/2", [:verbose] do
+    forall string <- binary do
+      #        test               metric
+      collect( is_binary(string), string |> byte_size |> to_range(10) )
+    end
+  end
+
+  defp to_range(b, a) do
+    low = div(b, a) * a
+
+    {low, low + a}
+  end
+
+  property "get map keys, incl. duplicates" do
+    forall kv <- list({key, val}) do
+      map = Map.new(kv)
+      for {key, _} <- kv, do: Map.fetch!(map, key)
+      collect(true, {:duplicates, kv
+                                  |> List.keysort(0)
+                                  |> E# L 2298
+      |> to_range(5) })
+    end
+  end
 end
