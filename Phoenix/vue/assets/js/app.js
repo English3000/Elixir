47c451f8a02f1bef17dd0eb0fd306ff7908af3d6
Alexander <english3000.org@gmail.com>
2018-11-30 08:16:04 -0800

    @ Ch. 1 HW

--- Phoenix/vue/assets/js/app.js
+++ Phoenix/vue/assets/js/app.js
@@ -3,3 +3,9 @@ import "phoenix_html"
 // Change to `vue.min.js` in prod
 import Counter from "./vues/counter.js"
 import Calculator from "./vues/calculator.js"
+
+import Vue from "vue/dist/vue.js"
+
+function b(){ return this.a + 1 }
+
+new Vue({ el: "#computed", data: {a: 4}, computed: {b} })
--- Phoenix/vue/assets/js/vues/calculator.js
+++ Phoenix/vue/assets/js/vues/calculator.js
@@ -3,19 +3,18 @@ import Vue from "vue/dist/vue.js"
 let data = {
   x:         null,
   y:         null,
-  z:         null,
   operation: "+"
 }
 
-function calculate(event){// @L 369
-  event.preventDefault()
-
-  switch(this.operation){
-    case "+": this.z = this.x + this.y ; break
-    case "-": this.z = this.x - this.y ; break
-    case "*": this.z = this.x * this.y ; break
-    case "/": this.z = this.x / this.y ; break
+function z(){
+  if (this.x && this.y) {
+    switch(this.operation){
+      case "+": return this.x + this.y
+      case "-": return this.x - this.y
+      case "*": return this.x * this.y
+      case "/": return this.x / this.y
+    }
   }
 }
 
-export default new Vue({ el: "#calculator", data, methods: {calculate} })
+export default new Vue({ el: "#calculator", data, computed: {z} })
