e040a3b7b624f35e1eeae762522e4941a8096661
Alexander <english3000.org@gmail.com>
2018-12-02 02:38:13 -0800

    Δ

--- Phoenix/vue/assets/js/app.js
+++ Phoenix/vue/assets/js/app.js
@@ -1,6 +1,2 @@
 import css from "../css/app.css"
 import "phoenix_html"
-// Change to `vue.min.js` in prod
-import Counter from "./vues/counter.js"
-import Calculator from "./vues/calculator.js"
-import Election from "./vues/election.js"
