8e6798ecee2738c0fd52ae9c0ba2e955f3765ce3
Alexander Marks-Katz <english3000.org@gmail.com>
2018-12-04 15:42:02 -0800

    Δ

--- Phoenix/vue/assets/js/app.js
+++ Phoenix/vue/assets/js/app.js
@@ -1,2 +1,20 @@
 import css from "../css/app.css"
 import "phoenix_html"
+import React, { useState } from "react"
+import { render } from "react-dom"
+
+// Input -> Storyboard, 26:30 ~ https://www.youtube.com/watch?v=dpw9EHDh2bM
+function Storyboard(){
+  const [query, setQuery] = useState("")
+
+  function handleChange(event){ setQuery(event.target.value) }
+
+  return (
+    <input type="text"
+           placeholder="Search for stories... with React Hooks!"
+           value={query}
+           onChange={handleChange}/>
+  )
+}
+
+render( <Storyboard/>, document.getElementById("react-storyboard") )
