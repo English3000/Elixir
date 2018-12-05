0d65b46b4ea63ce57ce966b03f58a1f1073db6e3
Alexander Marks-Katz <english3000.org@gmail.com>
2018-12-05 12:05:47 -0800

    + Storyboard works

--- Phoenix/vue/assets/js/app.js
+++ Phoenix/vue/assets/js/app.js
@@ -3,18 +3,43 @@ import "phoenix_html"
 import React, { useState } from "react"
 import { render } from "react-dom"
 
-// Input -> Storyboard, 26:30 ~ https://www.youtube.com/watch?v=dpw9EHDh2bM
-function Storyboard(){
-  const [query, setQuery] = useState("")
-
-  function handleChange(event){ setQuery(event.target.value) }
-
-  return (
-    <input type="text"
-           placeholder="Search for stories... with React Hooks!"
-           value={query}
-           onChange={handleChange}/>
-  )
+// really, this is simulating backend data, making it an unrealistic example...
+let reactUsers = {
+  Alex: ["I crashed my car today!",          "I ate someone's chocolate!" ] ,
+  John: ["Yesterday, someone stole my bag!", "Someone ate my chocolate..."]
 }
 
-render( <Storyboard/>, document.getElementById("react-storyboard") )
+function Storyboard({users}){
+  let [query, setQuery] = useState(""),
+      [data,  setData]  = useState(users)
+
+  function handleChange(event){
+    setQuery(event.target.value)
+
+    let newData = {}
+    for (const key in users) {
+      newData[key] = users[key].filter(
+                       story => story.includes(event.target.value)
+                     )
+    }
+
+    setData(newData)
+  }
+
+  return <div>
+           <input type="text"
+                  placeholder="Search for stories... with React Hooks!"
+                  value={query}
+                  onChange={handleChange}/>
+
+           <ul>{Object.entries(data).map(([name, stories]) =>
+             stories.map((story, index) =>
+               <li key={index}>
+                 {name} posted "{story}"
+               </li>
+             )
+           )}</ul>
+         </div>
+}
+
+render( <Storyboard users={reactUsers}/>, document.getElementById("react-storyboard") )
